825a2387ee1df175090d14cceb213dbe865e9589
=elangyudha280 <=elangyudharakasiwi2@gmail.com>
2024-08-28 21:24:05 -0700

    mulai desain page home

--- app/index.tsx
+++ app/index.tsx
@@ -3,18 +3,27 @@ import React from 'react'
 
 // import Component
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { ScrollView,Text,StatusBar } from 'react-native'
+import { ScrollView,Text,StatusBar,View } from 'react-native'
 import {StyledComponent} from 'nativewind'
+
+
 // import utils
 
 
 const HomePage = ()=>{
   return (
-    <SafeAreaView >
-        <StyledComponent component={ScrollView} className='relative w-full p-5 bg-red-500'>
-            <Text className="text-red-500">hello workd</Text>
+  
+    <>
+      <SafeAreaView>
+        <StyledComponent component={ScrollView} className='relative w-full  bg-red-500 h-full'>
+          <View className={'relative w-full p-5 bg-cyan-500'}>
+
+          </View>
         </StyledComponent>
-    </SafeAreaView>
+      </SafeAreaView>
+
+        <StatusBar  barStyle={'dark-content'} translucent={false}/>
+    </>
   )
 }
 
